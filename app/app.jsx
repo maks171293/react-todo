283cc575feeec029490a24ad00d7bd57a560d04f
Maks <andreev171293@gmail.com>
2017-04-11 10:47:43 +0300

    initial commit

---
+++ app/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+
+import 'style!css!foundation-sites/dist/css/foundation.min.css';
+$(document).foundation();
+
+import 'style!css!sass!applicationStyles';
+
+ReactDOM.render(
+<p>Hello</p>,
+  document.getElementById('app')
+);
